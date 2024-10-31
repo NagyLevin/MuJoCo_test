545140dcb3717cb3b1456ff1ce355e2d25f8cf0b
Levin <levsza13@gmail.com>
2024-10-31 01:43:08 +0100

    testing robosuite started to build a robosuite project

---
+++ RobotDoorRL/main.py
@@ -0,0 +1,34 @@
+import time
+import os
+import gym
+import pybullet_envs
+import numpy as np
+import datetime
+from torch.utils.tensorboard import SummaryWriter #for logging data
+import robosuite as suite
+# from robosuite_environment import RoboSuiteWrapper
+from robosuite.wrappers import GymWrapper
+
+
+if __name__ == '__main__':
+    env_name = "DoorRobotRL"
+
+    env = suite.make(
+        env_name,  # Environment
+        robots=["Panda"],  # Use two Panda robots # type of robot
+        controller_configs=suite.load_controller_config(default_controller="JOINT_VELOCITY"),  # Controller #pass joint vel
+        # controller_configs=suite.load_controller_config(default_controller="OSC_POSE"),
+        has_renderer=False,  # Enable rendering
+        use_camera_obs=False,
+        horizon=300, #time it takes the robot to figure out a solution DEPENDS ON PROBLEM
+        #render_camera= "frontview", #"sideview",           # Camera view
+        #has_offscreen_renderer=True,        # No offscreen rendering
+        reward_shaping=True, #on false it only gives reward when the task is fulfilled maks learning harder
+        control_freq=20,  # Control frequency
+    )
+    env = GymWrapper(env) #fits robosuite env into gym framework
+
+
+
+
+    env.close()
